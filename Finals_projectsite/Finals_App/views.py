30e8cb76ad469eb52326c8ff9312d6c8f45fc301
AllenFernandez <202380211@psu.palawan.edu.ph>
2026-05-27 14:47:04 +0800

    Prepared static and basic template files

--- Finals_projectsite/Finals_App/views.py
+++ Finals_projectsite/Finals_App/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from Finals_App.models import Semester
 
-# Create your views here.
+class SemesterListView(ListView):
+    model = Semester
+    template_name = 'home.html'
+    context_object_name = 'semesters'
--- Finals_projectsite/Finals_projectsite/urls.py
+++ Finals_projectsite/Finals_projectsite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Finals_App.views import SemesterListView
+from Finals_App import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', SemesterListView.as_view(), name='semester-list'),
 ]
