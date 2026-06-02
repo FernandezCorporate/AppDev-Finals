74f9df213ce30c528312179c94eafec63482df75
alliah2025 <202380062@psu.palawan.edu.ph>
2026-06-02 09:43:11 +0800

    added new html files and fix semester_detail.html

--- Finals_projectsite/Finals_projectsite/urls.py
+++ Finals_projectsite/Finals_projectsite/urls.py
@@ -26,4 +26,10 @@ urlpatterns = [
     path('semester/create/', views.SemesterCreateView.as_view(), name='semester-create'),
     path('semester/<int:pk>/update/', views.SemesterUpdateView.as_view(), name='semester-update'),
     path('semester/<int:pk>/delete/', views.SemesterDeleteView.as_view(), name='semester-delete'),
+    path('enrolled/create/', views.EnrolledCreateView.as_view(), name='enrolled-create'),
+    path('enrolled/<int:pk>/update/', views.EnrolledUpdateView.as_view(), name='enrolled-update'),
+    path('enrolled/<int:pk>/delete/', views.EnrolledDeleteView.as_view(), name='enrolled-delete'),
+    path('schedule/create/', views.ScheduleCreateView.as_view(), name='schedule-create'),
+    path('schedule/<int:pk>/update/', views.ScheduleUpdateView.as_view(), name='schedule-update'),
+    path('schedule/<int:pk>/delete/', views.ScheduleDeleteView.as_view(), name='schedule-delete'),
 ]
