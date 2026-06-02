8710e4c41265ebea24f9f1987f40fb13a04fde43
alliah2025 <202380062@psu.palawan.edu.ph>
2026-06-02 13:21:36 +0800

    Add frontend templates and update forms with Bootstrap styling

--- Finals_projectsite/Finals_App/forms.py
+++ Finals_projectsite/Finals_App/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm, inlineformset_factory
 from .models import Enrolled, Schedule
 
@@ -5,23 +6,55 @@ from .models import Enrolled, Schedule
 class EnrolledForm(ModelForm):
     class Meta:
         model = Enrolled
-        fields = [
-            'course',
-            'teacher_fname',
-            'teacher_lname',
-            'final_grade'
-        ]
+        fields = ['course', 'teacher_fname', 'teacher_lname', 'final_grade']
+        widgets = {
+            'course': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+            'teacher_fname': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'e.g. Juan'
+            }),
+            'teacher_lname': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'e.g. Dela Cruz'
+            }),
+            'final_grade': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'e.g. 1.25',
+                'step': '0.01'
+            }),
+        }
+
+
+class ScheduleForm(forms.ModelForm):
+    class Meta:
+        model = Schedule
+        fields = ['day_of_week', 'start_time', 'end_time', 'room']
+        widgets = {
+            'day_of_week': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+            'start_time': forms.TimeInput(attrs={
+                'class': 'form-control',
+                'type': 'time'
+            }),
+            'end_time': forms.TimeInput(attrs={
+                'class': 'form-control',
+                'type': 'time'
+            }),
+            'room': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'e.g. Room 101'
+            }),
+        }
 
 
 ScheduleFormSet = inlineformset_factory(
     Enrolled,
     Schedule,
-    fields=[
-        'day_of_week',
-        'start_time',
-        'end_time',
-        'room'
-    ],
+    form=ScheduleForm,      
+    fields=['day_of_week', 'start_time', 'end_time', 'room'],
     extra=1,
     can_delete=True
 )
